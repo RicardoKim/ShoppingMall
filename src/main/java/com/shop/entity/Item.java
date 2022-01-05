49b083a468909091bdfdf3ae601862c4f694d546
RicardoKim <may3210@naver.com>
2022-01-05 14:09:48 +0900

    [ADD] Add Item Entity, (2-3 상품 엔티티 설계)

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,4 +1,40 @@
 package com.shop.entity;
 
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.tomcat.jni.Local;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item")
+@Getter
+@Setter
+@ToString
 public class Item {
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus; //상품이 판매되고 있는지 아닌지를 판단할 때 사용하는 변수
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
 }
